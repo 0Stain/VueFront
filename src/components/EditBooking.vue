846f5578ae48747d55de1c7c0ef67a74fbad72d1
0Stain <louay.jeddou@esprit.tn>
2022-08-08 11:18:37 +0100

    Edit page update
    Edit page dynamically fetches data by Id.

--- src/components/EditBooking.vue
+++ src/components/EditBooking.vue
@@ -17,23 +17,24 @@
                 
                 <div class="form-group">
                     <label class="form-label mt-4">Booking Id</label>
-                    <input type="text" class="form-control" v-model="id" placeholder="Enter  Id">
+                    <input type="text" class="form-control" 
+                    v-model="booking.id" placeholder="Enter  Id">
                 </div>
                 <div class="form-group">
                     <label class="form-label mt-4">Start Date</label>
-                    <input type="date" class="form-control" v-model="t_start" placeholder="Enter Start Date">
+                    <input type="date" class="form-control" v-model="booking.t_start" placeholder="Enter Start Date">
                 </div>
                 <div class="form-group">
                     <label class="form-label mt-4">End Date</label>
-                    <input type="date" class="form-control" v-model="t_end" placeholder="Enter End Date">
+                    <input type="date" class="form-control" v-model="booking.t_end" placeholder="Enter End Date">
                 </div>
                 <div class="form-group">
                     <label class="form-label mt-4">User Id</label>
-                    <input type="text" class="form-control" v-model="user_id" placeholder="Enter User Id">
+                    <input type="text" class="form-control" v-model="booking.user_id" placeholder="Enter User Id">
                 </div>
                 <div class="form-group">
                     <label class="form-label mt-4">Box Id</label>
-                    <input type="text" class="form-control" v-model="box_id" placeholder="Enter Box Id">
+                    <input type="text" class="form-control" v-model="booking.box_id" placeholder="Enter Box Id">
                 </div>
                 <button class="btn btn-primary mt-4">Submit</button>
             </fieldset>
@@ -53,6 +54,7 @@ export default {
     name: 'EditBooking',
     data() {
         return {
+            booking: {},
             id: '',
             t_start: '',
             t_end: '',
@@ -71,8 +73,7 @@ export default {
 
         async getBookingById() {
                 
-            let url= `http://127.0.0.1:8000/api/geditBooking/${this.
-            $route.params.id}`;
+            let url= `http://127.0.0.1:8000/api/geditBooking/${this.$route.params.id}`;
             await axios.get(url).then(response => {
                 console.log(response);
                 this.booking = response.data;
